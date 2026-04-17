ffc6d1c315a5dcd59eec3278bdc933220a6d1db1
skanda shyam nadig <bostonblrboy@gmail.com>
2026-04-17 21:39:24 +0530

    Phase 4: Add error message for commit_serialize failure in commit_create

--- commit.c
+++ commit.c
@@ -221,7 +221,10 @@ int commit_create(const char *message, ObjectID *commit_id_out) {
     // 4. Serialize and write the commit object
     void *commit_data;
     size_t commit_len;
-    if (commit_serialize(&c, &commit_data, &commit_len) != 0) return -1;
+    if (commit_serialize(&c, &commit_data, &commit_len) != 0) {
+        fprintf(stderr, "error: failed to serialize commit\n");
+        return -1;
+    }
 
     if (object_write(OBJ_COMMIT, commit_data, commit_len, commit_id_out) != 0) {
         free(commit_data);
